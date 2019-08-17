73c374d36cb07fb2bb967dd727e47d46535f757a
YONY QUISPE RAMOS <a1510240@iberotec.edu.pe>
2019-08-16 21:02:54 -0500

    sad

---
+++ data.sql
@@ -0,0 +1,6 @@
+insert into prueba(nombre,dni) values ("Mátias bribío",3421);
+insert into prueba(nombre,dni) values ("Juán ñaño",34);
+insert into prueba(nombre,dni) values ("Teófilo  nñaña",324);
+insert into prueba(nombre,dni) values ("Ma'cos sunña",34);
+insert into prueba(nombre,dni) values ("óiasd ñbío",23);
+insert into prueba(nombre,dni) values ("Máññias Ríchapa",45);
